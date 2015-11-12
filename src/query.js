5efd2df2aa884056ee149fd06328fb8ed028d07f
stephanos <github@stephanbehnke.de>
2015-11-12 19:59:22 +0100

    add 'query' decorator

---
+++ src/query.js
@@ -0,0 +1,11 @@
+import ComponentUtil from './component';
+
+
+function query(namespace, id) {
+  return (target) => {
+    ComponentUtil.create(target, namespace, id, 'Query');
+  };
+}
+
+
+export default query;
---
+++ src/query.spec.js
@@ -0,0 +1,15 @@
+import assert from 'assert';
+
+import query from './query';
+import ComponentUtil from './component';
+
+
+describe('Query', () => {
+  it('should succeed', () => {
+    class Query {
+    }
+
+    query('query', 'success')(Query);
+    assert.ok(ComponentUtil.isValid(Query));
+  });
+});
