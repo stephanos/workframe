783c0bc7c2bbded32a82e16f8287a67bdd9a1722
stephanos <github@stephanbehnke.de>
2016-02-16 21:13:53 +0100

    remove unncessary file

--- src/internal/data/errors.js
+++
@@ -1,9 +0,0 @@
-export class CreateDataError extends Error {
-
-  constructor(details) {
-    const message = `invalid data: ${details}`;
-    super(message);
-    this.message = message;
-    this.name = 'ComponentError';
-  }
-}
