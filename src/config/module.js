3dda0254a4d6947617338d88c79aeb43cd99ff22
stephanos <github@stephanbehnke.de>
2016-06-03 15:00:26 +0200

    add config component

---
+++ src/config/module.js
@@ -0,0 +1,23 @@
+import { Component } from '../container';
+
+
+@Component()
+class Module {
+
+  async start() {
+    /* eslint global-require: 0 */
+    process.env.SUPPRESS_NO_CONFIG_WARNING = true;
+    this.config = require('config');
+  }
+
+  get(key) {
+    return this.config.get(key);
+  }
+
+  has(key) {
+    return this.config.has(key);
+  }
+}
+
+
+export default Module;
