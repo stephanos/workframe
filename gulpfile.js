3ca4ab2a59b546702685a4da2e09b1906a4f6fda
stephanos <github@stephanbehnke.de>
2016-07-21 21:13:39 +0200

    move IT outside of source dir

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('copy', () =>
 );
 
 gulp.task('build', () =>
-  gulp.src(['src/**/*.js', '!src/__tests/**'])
+  gulp.src(['src/**/*.js'])
     .pipe(cache('dist'))
     .pipe(sourcemaps.init())
     .pipe(babel(babelConf))
@@ -73,7 +73,7 @@ gulp.task('build', () =>
 );
 
 gulp.task('build-it', () =>
-  gulp.src(['src/__tests/**/*.js', '!src/**/*.t.js'])
+  gulp.src(['it/**/*.js', '!src/**/*.t.js'])
     .pipe(cache('dist'))
     .pipe(sourcemaps.init())
     .pipe(babel(babelConf))
@@ -81,7 +81,7 @@ gulp.task('build-it', () =>
       handleError(err.message + "\n" + err.codeFrame);
     })
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/__tests'))
+    .pipe(gulp.dest('dist/__it'))
 );
 
 gulp.task('lint', () =>
@@ -107,14 +107,14 @@ gulp.task('generate', () =>
 );
 
 gulp.task('unit-test', (done) => {
-  gulp.src(['dist/**/*.js', '!dist/**/*.spec.js', '!dist/__tests/**/*'])
+  gulp.src(['dist/**/*.js', '!dist/**/*.spec.js', '!dist/__it/**/*'])
     .pipe(istanbul({
       instrumenter: isparta.Instrumenter,
       includeUntested: true,
     }))
     .pipe(istanbul.hookRequire())
     .on('finish', () => {
-      gulp.src(['dist/**/*.spec.js', '!dist/__tests/**/*'], { read: false })
+      gulp.src(['dist/**/*.spec.js', '!dist/__it/**/*'], { read: false })
         .pipe(espower())
         .pipe(mocha({
           ui: 'bdd',
@@ -134,7 +134,7 @@ gulp.task('unit-test', (done) => {
 });
 
 gulp.task('integration-test', (done) => {
-  gulp.src(['dist/__tests/**/*.spec.js'], { read: false })
+  gulp.src(['dist/__it/**/*.spec.js'], { read: false })
     .pipe(espower())
     .pipe(mocha({
       ui: 'bdd',
