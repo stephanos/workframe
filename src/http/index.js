debf5927c840bc33c45bea55688f2fb8a60ea985
stephanos <github@stephanbehnke.de>
2016-07-24 18:58:14 +0200

    remove unused code from HTTP module

--- src/http/index.js
+++ src/http/index.js
@@ -1,12 +1,10 @@
 import Method from './methods';
-import HttpModule from './module';
 import Request from './request';
 import { Resource } from './decorators';
 import Response from './response';
 
 
 export {
-  HttpModule,
   Method,
   Request,
   Resource,
